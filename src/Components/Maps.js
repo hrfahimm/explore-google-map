6c4c17b7627bc5c24d63786f59a9bc59634dd3bb
hrfahimm <fahimtaj10@gmail.com>
2023-04-28 20:31:28 +0600

    'fast'

---
+++ src/Components/Maps.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: 22.941388,
+  lng: 90.874521,
+};
+
+const Maps = () => {
+  return (
+    <div>
+      <h2>My google map</h2>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+};
+
+export default Maps;
---
+++ src/Components/map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const map = () => {
+  return (
+    <div> </div>
+  )
+}
+
+export default map
